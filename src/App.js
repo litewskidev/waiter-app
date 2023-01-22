7d4b403e25a33c26d9974c8a1b45e655758083ad
Adrian Litewski <litewskidev@gmail.com>
2023-01-22 13:08:27 +0100

    server connection

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import Home from './components/pages/Home/Home';
 import { Container } from 'react-bootstrap';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import { fetchTables } from './redux/tablesRedux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(fetchTables()), [dispatch]);
+
   return(
     <Container>
       <Header />
