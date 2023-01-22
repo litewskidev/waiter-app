fd93d7c4d30ac469631f455fd447453700a8f5d5
Adrian Litewski <litewskidev@gmail.com>
2023-01-22 23:31:20 +0100

    remove status from server and add to const file

--- src/redux/statusRedux.js
+++ src/redux/statusRedux.js
@@ -1,27 +1,6 @@
-// SELECTORS
-export const getAllStatuses = ({ status }) => status;
-
-// ACTIONS
-const createActionName = actionName => `app/status/${actionName}`;
-const UPDATE_STATUS = createActionName('UPDATE_STATUS');
-
-// ACTION CREATORS
-export const updateStatus = payload => ({ type: UPDATE_STATUS, payload });
-export const fetchStatus = () => {
-  return(dispatch) => {
-    fetch('http://localhost:3131/api/status')
-      .then(res => res.json())
-      .then(status => dispatch(updateStatus(status)));
-  };
+export const statuses = {
+  free: "Free",
+  busy: "Busy",
+  reserved: "Reserved",
+  cleaning: "Cleaning",
 };
-
-const statusReducer = (statePart = [], action) => {
-  switch (action.type) {
-    case UPDATE_STATUS:
-      return [...action.payload];
-    default:
-      return statePart;
-  };
-};
-
-export default statusReducer;
